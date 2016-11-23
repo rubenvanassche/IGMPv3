f3f862d4e714d3e2f5ee1f1864a03e51f62d4dcc
Ruben Van Assche <rubenvanassche@gmail.com>
2016-11-23 11:57:52 +0100

    First packets

---
+++ elements/packets.hh
@@ -0,0 +1,32 @@
+#ifndef CLICK_PACKETS_HH
+#define CLICK_PACKETS_HH
+
+// Protocol Headers
+struct igmpv3query {
+  uint8_t type;
+  uint8_t max_response_code;
+  uint16_t checksum;
+  uint32_t group_address;
+  uint8_t resv_and_s_and_qrv;
+  uint8_t qqic;
+  uint16_t no_of_sources;
+};
+
+// see RFC 3376 for details
+struct grouprecord {
+  uint8_t type;
+  uint8_t aux_data_len;
+  uint16_t no_of_sources;
+  uint32_t multicast_address;
+  unsigned int sources[1];
+};
+
+struct igmpv3report {
+  uint8_t type;
+  uint8_t reserved;
+  uint16_t checksum;
+  uint16_t reserved_short;
+  uint16_t no_of_grouprecords;
+};
+
+#endif
